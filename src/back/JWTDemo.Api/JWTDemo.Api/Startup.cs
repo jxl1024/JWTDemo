97659082e3c0bfa90b36de9432ee420b35965e23
jiangxl <jiangxl@hicore.com>
2021-08-06 15:09:10 +0800

    增加过期时间的限制

--- src/back/JWTDemo.Api/JWTDemo.Api/Startup.cs
+++ src/back/JWTDemo.Api/JWTDemo.Api/Startup.cs
@@ -39,11 +39,15 @@ namespace JWTDemo.Api
             {
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    ValidIssuer = config.Issuer,
-                    ValidAudience = config.Audience,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.IssuerSigningKey)),
-                    ClockSkew = TimeSpan.FromMinutes(1)
-
+                    ValidateIssuerSigningKey = true,//是否验证签名,不验证的画可以篡改数据，不安全
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.IssuerSigningKey)),//解密的密钥
+                    ValidateIssuer = true,//是否验证发行人，就是验证载荷中的Iss是否对应ValidIssuer参数
+                    ValidIssuer = config.Issuer,//发行人
+                    ValidateAudience = true,//是否验证订阅人，就是验证载荷中的Aud是否对应ValidAudience参数
+                    ValidAudience = config.Audience,//订阅人
+                    ValidateLifetime = true,//是否验证过期时间，过期了就拒绝访问
+                    ClockSkew = TimeSpan.Zero,//这个是缓冲过期时间，也就是说，即使我们配置了过期时间，这里也要考虑进去，过期时间+缓冲，默认好像是7分钟，你可以直接设置为0
+                    RequireExpirationTime = true,
                 };
             });
             #endregion
